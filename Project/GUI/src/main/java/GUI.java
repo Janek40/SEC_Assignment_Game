3e5788a6d70855ae6e20f26ea270fd6bf09f3ae8
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-14 18:50:22 +0800

    Some gui code to test

--- Project/GUI/src/main/java/GUI.java
+++ Project/GUI/src/main/java/GUI.java
@@ -1,4 +1,36 @@
-public class GUI
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application
 {
-    public GUI(){}
+    public GUI(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.setTitle("Hello world!");
+	Button btn = new Button();
+	btn.setText("Say herro there");
+	btn.setOnAction(new EventHandler<ActionEvent>()
+	{
+	    @Override
+	    public void handle(ActionEvent event)
+	    {
+	        System.out.println("Hello world!");
+	    }
+	});
+	
+	StackPane root = new StackPane();
+	root.getChildren().add(btn);
+	primaryStage.setScene(new Scene(root, 300, 250));
+	primaryStage.show();
+    }
 }
