69e81eeb67ff5b5ee8866b8a284a49bbdd83872a
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-15 16:20:53 +0800

    added more functionality

--- Project/PluginFinder/src/main/java/Mainf.java
+++ Project/PluginFinder/src/main/java/Mainf.java
@@ -1,13 +1,30 @@
-import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
 
 public class Mainf
 {
     public static void main(String[] args)
     {
-        List<String> places = new LinkedList<String>();
+        //directories to search
+        List<String> places = new ArrayList<String>(10);
 	places.add("plugins/");
-	PluginFinder pf = new PluginFinder(places);
-	pf.find();
-	System.out.println(pf.toString());
+        
+	//strings that must be present 
+	List<String> contains = new ArrayList<String>(10);
+	contains.add("Plugin");
+	contains.add(".class");
+
+	PluginFinder pf = new PluginFinder(places, contains);
+	try
+	{  
+	    //find them
+	    pf.find();
+	    System.out.println(pf.toString());
+	}
+	catch(IOException e)
+	{
+	    System.out.println(e);
+	}
     }
 }
