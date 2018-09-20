3cca8f134f16de9bfaa4e88699517d2499a21418
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-20 21:02:36 +0800

    Added in a back button

--- Project/QuizPlugins/DoctorWhoQuiz/src/main/java/DoctorWhoQuiz.java
+++ Project/QuizPlugins/DoctorWhoQuiz/src/main/java/DoctorWhoQuiz.java
@@ -2,21 +2,55 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Map;
+import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.Scene;
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class DoctorWhoQuiz extends QuizPlugin
 {
-
+    private final int WINDOW_X = 500;
+    private final int WINDOW_Y = 500;
+    
     public DoctorWhoQuiz()
     {
         super("DoctorWhoQuiz");
     }
 
     @Override
-    public void runQuiz()
+    public void runQuiz(Scene prevScene, Stage primaryStage)
     {
-        System.out.println("Doctor who Quiz running");
+	System.out.println("Doctor who Quiz running");
 	Thread t1 = new Thread(() ->
 	{
+	    GridPane newRoot = new GridPane();
+	    Platform.runLater(() ->
+	    {
+	        primaryStage.setTitle("Doctor who quiz");
+	        primaryStage.setScene(new Scene(newRoot, WINDOW_X, WINDOW_Y));
+	        primaryStage.show();
+	    });
+	    Button btn = new Button();
+	    btn.setText("Back");
+	    btn.setOnAction(new EventHandler<ActionEvent>()
+	    {
+	        @Override
+		public void handle(ActionEvent event)
+		{
+		    Platform.runLater(() ->
+		    {
+		        primaryStage.setScene(prevScene);
+		    });
+		}
+	    });
+	    Platform.runLater(() ->
+	    {
+	        newRoot.add(btn, 0, 0);
+	    });
+
 	    try
 	    {
 	        super.loadPlugins();
