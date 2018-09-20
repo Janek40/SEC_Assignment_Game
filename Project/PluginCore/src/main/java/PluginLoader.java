6f0d5c2b029ef6217b2a2591060422da954b4f5a
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-20 15:06:23 +0800

    Loads multiple plugins to a Map

--- Project/PluginCore/src/main/java/PluginLoader.java
+++ Project/PluginCore/src/main/java/PluginLoader.java
@@ -6,6 +6,25 @@ public class PluginLoader<E> extends ClassLoader
 {
     public PluginLoader(){}
     
+    
+    @SuppressWarnings("unchecked")
+    public Map<String, E> loadPlugins(List<String> locations) throws ClassNotFoundException
+    {
+        Map<String, E> plugins = new HashMap<String, E>();
+	int length = locations.size();
+        E plugin;
+	String location;
+	for(int i=0;i<length;i++)
+	{
+            location = locations.get(i);
+	    plugin = loadPlugin(location);
+	    String className = getClassName(plugin);
+	    plugins.put(className, plugin);
+	}
+	return plugins;
+    }
+
+    
     @SuppressWarnings("unchecked")
     public E loadPlugin(String fname) throws ClassNotFoundException
     {
@@ -20,4 +39,18 @@ public class PluginLoader<E> extends ClassLoader
 	    throw new ClassNotFoundException(String.format("Could not load '%s' : %s", fname, ex.getMessage()), ex);
 	}
     }
+
+    public String getClassName(E plugin)
+    {
+        String ans = "";
+	int cutOff = 7;
+	String currName = plugin.getClass().toString();
+        int length = currName.length();
+	for(int i=cutOff-1;i<length;i++)
+	{
+	    ans += currName.charAt(i);
+	}
+	return ans;
+    }
+
 }
