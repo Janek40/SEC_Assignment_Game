15f18c909be5d3bb6ae7fa2491af5d43846b144b
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-20 13:59:06 +0800

    moved error showing to a new class

---
+++ Project/Errors/src/main/java/ErrorGUI.java
@@ -0,0 +1,21 @@
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
+
+public class ErrorGUI
+{
+    public ErrorGUI()
+    {
+    }
+
+    public void showError(String error)
+    {
+        Platform.runLater(() ->
+	{
+	    Alert alert = new Alert(AlertType.ERROR, error, ButtonType.OK);
+	    alert.showAndWait();
+	});
+    }
+}
