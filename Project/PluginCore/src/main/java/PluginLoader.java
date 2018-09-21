587750860ff11c72927809974b14681240fdbad0
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-21 23:18:24 +0800

    plugin loader now works

--- Project/PluginCore/src/main/java/PluginLoader.java
+++ Project/PluginCore/src/main/java/PluginLoader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.net.MalformedURLException;
 import java.io.File; 
 
 public class PluginLoader<E> extends ClassLoader
@@ -15,34 +16,35 @@ public class PluginLoader<E> extends ClassLoader
     {
 	try
 	{
-	System.out.println("Loading: " + loc);
-	File jar = new File(loc);
-        ClassLoader loader = URLClassLoader.newInstance(
-	    new URL[] {jar.toURI().toURL()},
-	    getClass().getClassLoader()
-	);
-        Class<?> clazz = Class.forName(name, true, loader);
-	Class<? extends E> newClass = clazz.asSubclass(parentClass);
-	return (E)newClass.getDeclaredConstructor().newInstance();
+	    System.out.println("Loading: " + loc);
+	    File jar = new File(loc);
+            ClassLoader loader = URLClassLoader.newInstance(
+	        new URL[] {jar.toURI().toURL()},
+	        getClass().getClassLoader()
+	    );
+            Class<?> clazz = Class.forName(name, true, loader);
+	    Class<? extends E> newClass = clazz.asSubclass(parentClass);
+	    return (E)newClass.getDeclaredConstructor().newInstance();
 	}
-	catch(Exception e)
+	catch(MalformedURLException e)
 	{
-	    System.out.println(e.toString());
-	    throw new ClassNotFoundException("fuck", e);
+	    throw new ClassNotFoundException("Cannot load plugin, Given file location is invalid",e);
+	}
+	catch(NoSuchMethodException e)
+	{
+	    throw new ClassNotFoundException("Cannot load plugin, Given jar could not be created",e);
+	}
+	catch(InstantiationException e)
+	{
+	    throw new ClassNotFoundException("Cannot load plugin, is this an abstract class?",e);
+	}
+	catch(IllegalAccessException e)
+	{
+	    throw new ClassNotFoundException("Cannot load plugin, illegal access",e);
+	}
+	catch(InvocationTargetException e)
+	{
+	    throw new ClassNotFoundException("Cannot load plugin, unable to invoke",e);
 	}
     }
-    
-    public String getClassName(E plugin)
-    {
-        String ans = "";
-	int cutOff = 7;
-	String currName = plugin.getClass().toString();
-        int length = currName.length();
-	for(int i=cutOff-1;i<length;i++)
-	{
-	    ans += currName.charAt(i);
-	}
-	return ans;
-    }
-
 }
