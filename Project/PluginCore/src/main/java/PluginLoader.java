9aae0248add88d51e1d7589ef2834ac034877636
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-21 11:01:14 +0800

    Can now load plugins better!!!! they can have as many $$$$ as the want

--- Project/PluginCore/src/main/java/PluginLoader.java
+++ Project/PluginCore/src/main/java/PluginLoader.java
@@ -2,78 +2,36 @@ import java.nio.file.*;
 import java.util.*;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.io.File; 
 
 public class PluginLoader<E> extends ClassLoader
 {
     public PluginLoader(){}
     
-    
     @SuppressWarnings("unchecked")
-    public E loadPlugin(String loc) throws ClassNotFoundException
+    public E loadPlugin(String loc, String name, Class<E> parentClass) throws ClassNotFoundException
     {
-        try
+	try
 	{
-	    JarReader jreader = new JarReader();
-	    jreader.readJar(loc);
-	    List<byte[]> contents = jreader.getContents();
-	    List<String> names = jreader.getNames();
-	    
-	    int i;
-	    String base=null;
-	    for(i=0;i<names.size();i++)
-	    {
-	        base = names.get(i);
-	        if(!base.contains("$"))
-		{
-		    break;
-		}
-	    }
-            
-	    if(base==null)
-	    {
-	        return null;
-	    }
-
-            byte[] basePlugin = contents.remove(i);
-	    Class<?> cls = defineClass(null, basePlugin, 0, basePlugin.length);
-	    E obj = (E)cls.getDeclaredConstructor().newInstance();
-            
-	    for(i=0;i<contents.size();i++)
-	    {
-	        byte[] innerClass = contents.remove(i);
-	        Class<?> cls2 = defineClass(null, innerClass, 0, innerClass.length);
-                resolveClass(cls);
-	    }
-            System.out.println("    Loaded: " + obj.toString());
-	    return obj;
+	System.out.println("Loading: " + loc);
+	File jar = new File(loc);
+        ClassLoader loader = URLClassLoader.newInstance(
+	    new URL[] {jar.toURI().toURL()},
+	    getClass().getClassLoader()
+	);
+        Class<?> clazz = Class.forName(name, true, loader);
+	Class<? extends E> newClass = clazz.asSubclass(parentClass);
+	return (E)newClass.getDeclaredConstructor().newInstance();
 	}
-	//trying to load the abstract base class
-	catch(InstantiationException ex)
+	catch(Exception e)
 	{
-	    throw new ClassNotFoundException("You cannot load the base class!");
-	}
-	catch(IOException ex)
-	{
-	    throw new ClassNotFoundException("Unable to load the given plugin");
-	}
-	catch(NoSuchMethodException ex)
-	{
-	    throw new ClassNotFoundException("Unable to load, is it the base class?");
-	}
-	catch(IllegalAccessException ex)
-	{
-	    throw new ClassNotFoundException("You do not have the permissions to load this plugin");
-	}
-	catch(InvocationTargetException ex)
-	{
-	    throw new ClassNotFoundException("Unable to invoke the given plugin");
-	}
-	catch(java.lang.LinkageError ex)
-	{
-	    return null;
+	    System.out.println(e.toString());
+	    throw new ClassNotFoundException("fuck", e);
 	}
     }
-
+    
     public String getClassName(E plugin)
     {
         String ans = "";
