92e66c79cbaaf15ca6a1f49cdb60680a1cfe25be
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-21 11:00:22 +0800

    script moves compiled plugins to the correct folders before running

--- Project/plrun.sh
+++ Project/plrun.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "Moving Quizzes"
+cp QuizPlugins/DoctorWhoQuiz/build/libs/DoctorWhoQuiz.jar plugins/Quizzes/
+echo "Moving Question Types"
+cp QuestionTypes/MultiChoice/build/libs/MultiChoice.jar plugins/QuestionTypes/
+cp QuestionTypes/ShortAnswer/build/libs/ShortAnswer.jar plugins/QuestionTypes/
+echo "Running program"
 java -jar build/libs/janeks-TextGame.jar
