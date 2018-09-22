0d51a24567dd391dd54e01c28204c7e2e853224a
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-23 00:24:01 +0800

    added display result method

--- Project/QuizPlugins/QuizPlugin/src/main/java/QuizPlugin.java
+++ Project/QuizPlugins/QuizPlugin/src/main/java/QuizPlugin.java
@@ -6,12 +6,19 @@ import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
 import java.util.HashMap;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.application.Platform;
 
 
 public abstract class QuizPlugin
 {
     private String name;
     private Map<String, QuestionType> types = new HashMap<String, QuestionType>();
+    
+    protected volatile Object key = new Object();
+    protected volatile boolean stop = false;
 
     public QuizPlugin(String name)
     {
@@ -54,6 +61,18 @@ public abstract class QuizPlugin
 	    }
 	}
     }
+
+    protected void displayResult(String title, String header, String message)
+    {
+        Platform.runLater(() ->
+	{
+	    Alert alert = new Alert(AlertType.INFORMATION, message, ButtonType.OK);
+	    alert.setTitle(title);
+	    alert.setHeaderText(header);
+	    alert.showAndWait();
+	});
+    }
+
 
     protected QuestionType get(String key) throws ClassNotFoundException
     {
