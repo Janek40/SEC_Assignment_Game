be083dd5cb7fe1a30dab9d1a3aac4babc41d08d3
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-23 00:23:34 +0800

    tried to get working with futures

--- Project/QuizPlugins/DoctorWhoQuiz/src/main/java/DoctorWhoQuiz.java
+++ Project/QuizPlugins/DoctorWhoQuiz/src/main/java/DoctorWhoQuiz.java
@@ -9,12 +9,17 @@ import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
 
 public class DoctorWhoQuiz extends QuizPlugin
 {
     private final int WINDOW_X = 500;
     private final int WINDOW_Y = 500;
-    
+    private volatile LinkedBlockingQueue<Integer> score = new LinkedBlockingQueue<Integer>();
+
     public DoctorWhoQuiz()
     {
         super("DoctorWhoQuiz");
@@ -36,14 +41,28 @@ public class DoctorWhoQuiz extends QuizPlugin
 	    
 	    try
 	    {
-	        super.loadPlugins();
+	        int myScore = 0;
+		super.loadPlugins();
 	        QuestionType mcp = super.get("MultiChoice");
 		QuestionType sa = super.get("ShortAnswer");
                 
-		Question q1 = mcp.makeQuestion("What is my name?", new String[] { "Janek", "Tom" }, 0);
-
-		q1.invoke(20, primaryStage);
+		Question q1 = mcp.makeQuestion(score, "What is my name?", new String[] { "Janek", "Tom" }, 0);
+		Question q2 = mcp.makeQuestion(score, "What is my last name?", new String[] { "Joyce", "Scott" }, 0);
+                
+		try
+		{
+		    Future<Integer> q1Ans = q1.invoke(20, primaryStage, score);
+		    myScore += q1Ans.get();
+		    Future<Integer> q2Ans = q2.invoke(15, primaryStage, score);
+		    myScore += q2Ans.get();
+		}
+		catch(InterruptedException e)
+		{
+		    System.out.println("Interrupted! unable to add to your score");
+		}
+		displayResult("Results", "Doctor who quiz result", "You scored " + myScore + " out of a maximum " + 2 + " points!");
 		System.out.println("Done");
+		returnToMain(prevScene, primaryStage);
 	    }
             catch(IOException e)
 	    {
@@ -55,6 +74,13 @@ public class DoctorWhoQuiz extends QuizPlugin
             catch(ClassNotFoundException e)
 	    {
 	        ErrorGUI err = new ErrorGUI();
+		err.showError(e.getMessage());
+		returnToMain(prevScene, primaryStage);
+		return;
+	    }
+	    catch(ExecutionException e)
+	    {
+                ErrorGUI err = new ErrorGUI();
 		err.showError(e.getMessage());
 		returnToMain(prevScene, primaryStage);
 		return;
