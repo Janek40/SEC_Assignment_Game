647c8401ace92abe91b20f226d80d3f0cc61f2a2
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-25 08:19:54 +0800

    Quiz matches new format

--- Project/QuizPlugins/DoctorWhoQuiz/src/main/java/DoctorWhoQuiz.java
+++ Project/QuizPlugins/DoctorWhoQuiz/src/main/java/DoctorWhoQuiz.java
@@ -18,7 +18,8 @@ public class DoctorWhoQuiz extends QuizPlugin
 {
     private final int WINDOW_X = 500;
     private final int WINDOW_Y = 500;
-    //private volatile LinkedBlockingQueue<Integer> score = new LinkedBlockingQueue<Integer>();
+    //private volatile Boolean key = false;
+    private volatile SafeBoolean key = new SafeBoolean(false);
 
     public DoctorWhoQuiz()
     {
@@ -49,25 +50,28 @@ public class DoctorWhoQuiz extends QuizPlugin
 		Question q1 = mcp.makeQuestion("What is my name?", new String[] { "Janek", "Tom" }, 0);
 		Question q2 = mcp.makeQuestion("What is my middle name?", new String[] { "Bob", "Karl" }, 1);
 		Question q3 = mcp.makeQuestion("What is my last name?", new String[] { "Joyce", "Scott" }, 0);
+		Question q4 = mcp.makeQuestion("What is 2+2", new String[] { "4", "5", "6" }, 0);
                 
+		LinkedBlockingQueue<Integer> turn = new LinkedBlockingQueue<Integer>();
+
 		try
 		{
-		    Future<Integer> q1Ans = q1.invoke(0, primaryStage, false);
-		    q2.invoke(0, primaryStage, true);
-		    myScore += q1Ans.get();
+		    //time to complete, primary stage, list to keep track of turns, a key, the turn
+		    Future<Integer> q1Ans = q1.invoke(0, primaryStage, turn, key, 1);
+		    Future<Integer> q2Ans = q2.invoke(20, primaryStage, turn, key, 2);
+		    Future<Integer> q3Ans = q3.invoke(30, primaryStage, turn, key, 3);
+		    Future<Integer> q4Ans = q4.invoke(30, primaryStage, turn, key, 4);
 
-		    Future<Integer> q2Ans = q2.invoke(20, primaryStage, false);
-		    q3.invoke(0, primaryStage, true);
+		    myScore += q1Ans.get();
 		    myScore += q2Ans.get();
-		    
-		    Future<Integer> q3Ans = q3.invoke(30, primaryStage, false);
 		    myScore += q3Ans.get();
+		    myScore += q4Ans.get();
 		}
 		catch(InterruptedException e)
 		{
 		    System.out.println("Interrupted! unable to add to your score");
 		}
-		displayResult("Results", "Doctor who quiz result", "You scored " + myScore + " out of a maximum " + 3 + " points!");
+		displayResult("Results", "Doctor who quiz result", "You scored " + myScore + " out of a maximum " + 4 + " points!");
 		System.out.println("Done");
 		returnToMain(prevScene, primaryStage);
 	    }
