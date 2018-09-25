658c0f4d97147875b42c3ec2f188b109f711bba2
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-09-25 08:20:39 +0800

    compiled

--- Project/QuestionTypes/QuestionType/src/main/java/QuestionType.java
+++ Project/QuestionTypes/QuestionType/src/main/java/QuestionType.java
@@ -1,11 +1,12 @@
 import javafx.scene.layout.GridPane;
 import java.util.concurrent.LinkedBlockingQueue;
+import javafx.scene.control.Label;
 
 public abstract class QuestionType
 {
     private String name;
     public LinkedBlockingQueue<Integer> score = new LinkedBlockingQueue<Integer>();
-    
+
     public QuestionType(String inName)
     {
         this.name = inName;
