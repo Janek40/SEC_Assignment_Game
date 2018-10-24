dbb8bc131bbe9a470a4b6b52fc285ca7c664839d
Janek Joyce <janek.joyce@student.curtin.edu.au>
2018-10-24 20:45:44 +0800

    implemented ShortAnswer

--- Project/QuestionTypes/ShortAnswer/src/main/java/ShortAnswer.java
+++ Project/QuestionTypes/ShortAnswer/src/main/java/ShortAnswer.java
@@ -1,20 +1,134 @@
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class ShortAnswer extends QuestionType
 {
+    private final int WINDOW_X = 500;
+    private final int WINDOW_Y = 500;
+
     public ShortAnswer()
     {
         super("ShortAnswer");
     }
     
-    protected GridPane makePreview(String desc)
+    @Override
+    @SuppressWarnings("unchecked")
+    public Question makeQuestion(Object... args)
     {
-         return null;
+        String desc = (String)args[0];
+	String correct = (String)args[1];
+
+	GridPane regularRoot = makeQuestionActual(desc, correct);
+	GridPane previewRoot = makePreview(desc);
+	return new Question(regularRoot, previewRoot, super.getScore());
     }
 
     @Override
-    public Question makeQuestion(Object... args)
+    protected GridPane makePreview(String desc)
     {
-        return null;
+         GridPane root = new GridPane();
+	 Label descLabel = new Label(desc);
+	 descLabel.setText(desc);
+	 root.add(descLabel, 0, 0);
+	 return root;
     }
+
+    private volatile Object scoreKey = new Object();
+    private volatile int myScore = -1;
+
+    private GridPane makeQuestionActual(String desc, String answer)
+    {
+        GridPane root = new GridPane();
+	Button submitBtn = new Button();
+	Button nextBtn = new Button();
+	Button restartBtn = new Button();
+	Label descLabel = new Label(desc);
+        Label endMessage = new Label();
+
+        TextField textField = new TextField();
+	HBox hb = new HBox();
+	hb.getChildren().addAll(new Label("Answer..."), textField);
+	hb.setSpacing(10);
+
+	//dont forget a text field!
+        submitBtn.setText("Submit");
+	submitBtn.setUserData("SUBMIT");
+	submitBtn.setOnAction(new EventHandler<ActionEvent>()
+	{
+	    @Override
+	    public void handle(ActionEvent event)
+	    {
+	        String input = textField.getCharacters().toString();
+		String message = "";
+		if(input.equals(answer))
+		{
+		    myScore = 1;
+		    message = "Correct!";
+		}
+		else
+		{
+		    myScore = 0;
+		    message = "Wrong!";
+		}
+		endMessage.setText(message);
+		submitBtn.setDisable(true);
+		nextBtn.setDisable(false);
+	    }
+	});
+
+        nextBtn.setText("Next");
+	nextBtn.setUserData("NEXT");
+	nextBtn.setDisable(true);
+	nextBtn.setOnAction(new EventHandler<ActionEvent>()
+	{
+	    @Override
+	    public void handle(ActionEvent event)
+	    {
+	        try
+		{
+	            score.put(myScore);
+		}
+		catch(InterruptedException e)
+		{
+		    System.out.println("Interrupted!");
+		}
+	    }
+	});
+
+	restartBtn.setText("Exit Quiz");
+	restartBtn.setDisable(false);
+	restartBtn.setOnAction(new EventHandler<ActionEvent>()
+	{
+	    @Override
+	    public void handle(ActionEvent event)
+	    {
+	        try
+		{
+	            score.put(-1);   
+		}
+		catch(InterruptedException e)
+		{
+		    System.out.println("Unable to exit quiz, because I was exited?");
+		}
+	    }
+	});
+        
+	root.add(descLabel,  0,0);
+	root.add(hb,         0,1);
+	root.add(submitBtn,  0,2);
+	root.add(nextBtn,    0,3);
+	root.add(endMessage, 0,4);
+	root.add(restartBtn, 0,5);
+        return root;
+    }
+
+
 }
